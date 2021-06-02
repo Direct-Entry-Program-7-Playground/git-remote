e9503ec523e57fec84707f0b7d19f9d5f5d5163c
Manoj Randeni <randeni.ramr@gmail.com>
2021-06-02 12:21:15 +0530

    Add sone new lines to AppInitializer.java

--- AppInitializer.java
+++ AppInitializer.java
@@ -1,5 +1,7 @@
-public class AppInitializer() {
+public class AppInitializer {
     public static void main (String[] args){
+        System.out.println("-----------------");
         System.out.println("Hello Git Remotes!");
+        System.out.println("-----------------");
     }
 }
